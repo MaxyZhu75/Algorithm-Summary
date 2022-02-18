669dfd33e87f4c8b9773cca83f411347867f83a2
Max Zhu <zhumaxy@gmail.com>
2022-02-17 23:57:38 -0500

    Update solution.java

--- leetcode/494 Target Sum/solution.java
+++ leetcode/494 Target Sum/solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int findTargetSumWays(int[] nums, int target) { // 难点：将该问题转化为SubsetSum问题！！！
         int numsSum = sum(nums); // 该数组将被划分为正数组负数组两部分记作P和N，由于数组本身元素皆为非负数，则可推导 P-N=target；即 P-(numsSum-P)=target；即 P=(target+numsSum)/2！！！
         if (numsSum < Math.abs(target) || (numsSum + target) % 2 == 1) { // case1：注意target可能为负！！！
-            return 0; // case2：SubsetSum目标和明显需要是整数！！！
+            return 0;                                                    // case2：SubsetSum目标和明显需要是整数！！！
         }
         int M = (numsSum + target) / 2;
 
@@ -12,7 +12,7 @@ class Solution {
             for (int j=M; j>=0; j--) {
                 if (j >= num) { // dp方程类似硬币找零问题，都是统计多少种方法！！！
                     d[j] = d[j] + d[j-num]; // case1: d[i][j] = d[i-1][j] + d[i-1][j-num]！！！
-                } // case2: d[i][j] = d[i-1][j]！！！
+                }                           // case2: d[i][j] = d[i-1][j]！！！
             }
         }
         return d[M];
