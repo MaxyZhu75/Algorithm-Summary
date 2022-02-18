8c0f7d3dbde61e6d27d5eee147f3c667baebe88d
Max Zhu <zhumaxy@gmail.com>
2022-02-17 23:58:14 -0500

    Update solution.go

--- leetcode/494 Target Sum/solution.go
+++ leetcode/494 Target Sum/solution.go
@@ -1,7 +1,7 @@
 func findTargetSumWays(nums []int, target int) int { // 难点：将该问题转化为SubsetSum问题！！！
     numsSum := sum(nums) // 该数组将被划分为正数组负数组两部分记作P和N，由于数组本身元素皆为非负数，则可推导 P-N=target；即 P-(numsSum-P)=target；即 P=(target+numsSum)/2！！！
     if numsSum < abs(target) || (numsSum + target) % 2 == 1 { // case1：注意target可能为负！！！
-        return 0 											 // case2：SubsetSum目标和明显需要是整数！！！
+        return 0 // case2：SubsetSum目标和明显需要是整数！！！
     }
     M := (numsSum + target) / 2
 
@@ -11,7 +11,7 @@ func findTargetSumWays(nums []int, target int) int { // 难点：将该问题转
         for j:=M; j>=0; j-- {
             if j >= num { // dp方程类似硬币找零问题，都是统计多少种方法！！！
                 d[j] = d[j] + d[j-num] // case1: d[i][j] = d[i-1][j] + d[i-1][j-num]！！！
-            }						  // case2: d[i][j] = d[i-1][j]！！！
+            } // case2: d[i][j] = d[i-1][j]！！！
         }
     }
 
