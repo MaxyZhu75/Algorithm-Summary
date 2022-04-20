fde93edd03e6cd1b2c6cd21d24dab02bc0dc342c
Max Zhu <zhumaxy@gmail.com>
2022-04-20 10:01:41 -0400

    Update solution.go

--- leetcode/15 3Sum/solution.go
+++ leetcode/15 3Sum/solution.go
@@ -1,5 +1,5 @@
 func threeSum(nums []int) [][]int { // 类似题目为四数之和！！！
-	result := [][]int{} // p1，left，right指向的值可以相同，但不能重复往result添加已存在的答案组合！！！
+    result := [][]int{} // p1，left，right指向的值可以相同，但不能重复往result添加已存在的答案组合！！！
     n := len(nums)
     if (n < 3) {
         return result
@@ -7,11 +7,11 @@ func threeSum(nums []int) [][]int { // 类似题目为四数之和！！！
 
     sort.Ints(nums) // 先排序！！！
 
-	for p1:=0; p1<n-2; p1++ {
+    for p1:=0; p1<n-2; p1++ {
         if p1 > 0 && nums[p1] == nums[p1-1] { // 记住三个剪枝操作，加强代码效率！！！
             continue;
         }
-		if nums[p1] + nums[p1+1] + nums[p1+2] > 0 {
+        if nums[p1] + nums[p1+1] + nums[p1+2] > 0 {
             break;
         }
         if nums[p1] + nums[n-1] + nums[n-2] < 0 {
@@ -43,6 +43,6 @@ func threeSum(nums []int) [][]int { // 类似题目为四数之和！！！
                 right--
             }
         }
-	}
-	return result
+    }
+    return result
 }
