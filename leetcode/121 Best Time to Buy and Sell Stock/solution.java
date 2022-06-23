d59c270fb68164633e8b29fccc472838a38ceab2
Max Zhu <zhumaxy@gmail.com>
2022-06-23 16:24:41 -0400

    Update solution.java

--- leetcode/121 Best Time to Buy and Sell Stock/solution.java
+++ leetcode/121 Best Time to Buy and Sell Stock/solution.java
@@ -4,8 +4,8 @@ class Solution {
         if (n == 0) return 0;
         int currentMin = prices[0]; // 初始值为第一天股价！！！
         int maxProfit = 0;
-        for(int i=0; i<n; i++){
-            if( prices[i] < currentMin ) {
+        for (int i=0; i<n; i++) {
+            if (prices[i] < currentMin) {
                 currentMin = prices[i]; // if-else 结构即买与卖，且满足买和卖不能在同一天操作的条件！！！
             } else {
                 maxProfit = Math.max(maxProfit, prices[i]-currentMin);
