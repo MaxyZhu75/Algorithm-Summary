edb9e350c64c0bbf3a324c870806b24c5f323b20
Max Zhu <zhumaxy@gmail.com>
2022-06-25 09:13:52 -0400

    Update solution.go

--- leetcode/227 Basic Calculator II/solution.go
+++ leetcode/227 Basic Calculator II/solution.go
@@ -1,26 +1,26 @@
 func calculate(s string) int {
     n := len(s)
     preOperand := '+'
-    digit := 0 // 待处理数字！！！
+    num := 0 // 待处理数字！！！
     myStack := []int{}
     for i, letter := range s {
         isDigit := (letter >= '0') && (letter <= '9') // 当前字符为数字；记住该统计数字的操作（套路）！！！
         if isDigit {
-            digit = digit*10 + int(letter-'0')
+            num = num*10 + int(letter-'0')
         }
         if !isDigit && letter != ' ' || i == n-1 { // 当前字符为「操作符」或「最后一位」！！！
             switch preOperand { // 注意此处是在对「前一个操作符」进行运算！！！
                 case '+':
-                    myStack = append(myStack, digit)
+                    myStack = append(myStack, num)
                 case '-':
-                    myStack = append(myStack, -digit)
+                    myStack = append(myStack, -num)
                 case '*':
-                    myStack[len(myStack)-1] *= digit
+                    myStack[len(myStack)-1] *= num
                 default:
-                    myStack[len(myStack)-1] /= digit
+                    myStack[len(myStack)-1] /= num
             }
         preOperand = letter
-        digit = 0 // 待处理数字「归零」！！！
+        num = 0 // 待处理数字「归零」！！！
         }
     }
 
