532ef68367f3101c5e3ef79c49c0b619254eac99
Max Zhu <zhumaxy@gmail.com>
2022-07-22 10:11:38 -0400

    Update solution.java

--- leetcode/273 Integer to English Words/solution.java
+++ leetcode/273 Integer to English Words/solution.java
@@ -1,27 +1,28 @@
 class Solution {
-	private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-	private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
-	public String numberToWords(int num) {
-		if (num == 0) return "Zero";
-		return recursion(num);
-	}
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+        return recursion(num);
+    }
 
-	String recursion(int num) {
-		String result = "";
-		if (num < 20) {
-			result = LESS_THAN_20[num];
-		} else if (num < 100) {
-			result = TENS[num/10] + " " + recursion(num%10);
-		} else if (num < 1000) {
-			result = recursion(num/100) + " Hundred " + recursion(num%100);
-		} else if (num < 1000000) {
-			result = recursion(num/1000) + " Thousand " + recursion(num%1000);
-		} else if (num < 1000000000) {
-			result = recursion(num/1000000) + " Million " + recursion(num%1000000);
-		} else {
-			result = recursion(num/1000000000) + " Billion " + recursion(num%1000000000);
-		}
-		return result.trim(); // trim()能去除首位空格！！
-	}
+    String recursion(int num) {
+    String result = "";
+    if (num < 20) {
+	result = LESS_THAN_20[num];
+    } else if (num < 100) {
+	result = TENS[num/10] + " " + recursion(num%10);
+    } else if (num < 1000) {
+	result = recursion(num/100) + " Hundred " + recursion(num%100);
+    } else if (num < 1000000) {
+	result = recursion(num/1000) + " Thousand " + recursion(num%1000);
+    } else if (num < 1000000000) {
+	result = recursion(num/1000000) + " Million " + recursion(num%1000000);
+    } else {
+	result = recursion(num/1000000000) + " Billion " + recursion(num%1000000000);
+    }
+
+    return result.trim(); // trim()能去除首位空格！！
+    }
 }
